ba754c79ef183ff6ee40be2793d2356123cbc811
stinkyskate <stinkyskate@pm.me>
2021-09-22 16:51:48 +0100

    only check when needed

--- vpn_check.py
+++ vpn_check.py
@@ -11,15 +11,19 @@ def get_current_ip():
     return urllib.request.urlopen("https://ipinfo.io/ip").read().decode()
 
 
-def kill_process(proc):
-        from os import kill
-        from signal import SIGKILL
-        from subprocess import check_output, CalledProcessError
-        try:
-            kill(int(check_output(["pidof", proc])), SIGKILL)
-            print("killed the offender")
-        except CalledProcessError:
-            print("no worries")
+def get_pid(proc):
+    from subprocess import check_output, CalledProcessError
+    try:
+        return int(check_output(["pidof", proc]))
+    except CalledProcessError:
+        return None
+
+
+def kill_process(pid):
+    from os import kill
+    from signal import SIGKILL
+    kill(pid, SIGKILL)
+    print("killed the offender")
 
 
 def is_safe():
@@ -29,8 +33,10 @@ def is_safe():
 
 
 def main():
-    if not is_safe():
-        kill_process("qbittorrent-nox")
+    pid = get_pid("qbittorrent-nox")
+    if pid:
+        if not is_safe():
+            kill_process(pid)
 
 
 if __name__ == "__main__":
